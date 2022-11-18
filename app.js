c54f51675719214428b55477bf10f9ed9b7df6ef
DNS1196 <107742954+DNS1196@users.noreply.github.com>
2022-11-18 02:58:36 -0500

    Object Enhancement EX

--- app.js
+++ app.js
@@ -1,33 +1,22 @@
-
-// function filterOutOdds() {
-// 	var nums = Array.prototype.slice.call(arguments);
-// 	return nums.filter(function (num) {
-// 		return num % 2 === 0
-// 	});
-// }
-
-const filterOutOdds = (...args) => args.filter(v => v % 2 === 0)
-
-const findMin = (...nums) => Math.min(...nums)
-
-const mergeObjects = (firstObj, secondObj) => ({ ...firstObj, ...secondObj })
-
-const doubleAndReturnArgs = (arr, ...args) => ([...arr, ...args.map(n => n * 2)])
-
-const removeRandom = (items) => {
-	let i = Math.floor(Math.random() * items.length);
-	return [...items.slice(0, i), ...items.slice(i + 1)]
+const createInstructor = (firstName, lastName) => {
+    return { firstName, lastName };
 }
 
-const extend = (arr1, arr2) => ([...arr1, ...arr2])
-
-const addKeyVal = (obj, key, val) => ({ ...obj, [key]: val })
-
-const removeKey = (obj, key) => {
-	({ [key]: undefined, ...obj } = obj);
-	return obj;
+let favoriteNumber = 42;
+const teacher = {
+    firstName: "Colt",
+    [favoriteNumber]: "That is my favorite!"
 }
 
-const combine = (obj1, obj2) => ({ ...obj1, ...obj2 })
+const instructor = {
+    firstName: 'Colt',
+    sayHi() { return "Hi!" },
+    sayBye() { return this.firstName + ' says bye!' }
+}
 
-const update = (obj, key, val) => ({ ...obj, [key]: val })
+const createAnimal = (species, func, noise) => {
+    return {
+        species,
+        [func]() { return noise; }
+    }
+}
